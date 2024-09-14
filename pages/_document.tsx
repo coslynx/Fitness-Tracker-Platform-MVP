9df09b93c6063c2453dc599f2816fb63c026cbe5
CosLynx AI <drixisgod@gmail.com>
2024-09-14 21:22:07 +0530

    generated file: `pages/_document.tsx`

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:wght@400;700&display=swap"
+        />
+        <link rel="stylesheet" href="/globals.css" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
